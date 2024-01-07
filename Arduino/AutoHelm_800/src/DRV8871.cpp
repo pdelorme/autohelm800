e715bbca96244838ea95c801e758b7c08394f173
patrice delorme <patrice.delorme@gmail.com>
2024-01-07 08:56:00 +0100

    Proportional PWM

--- Arduino/AutoHelm_800/src/DRV8871.cpp
+++ Arduino/AutoHelm_800/src/DRV8871.cpp
@@ -25,22 +25,21 @@ class DRV8871 {
     /**
      * Move id=n direction except if lockeded by endStop
      */
-    void doMove(int dir){
-      if(dir!=0){
-        dir=dir>0?+1:-1;
-        if(endstops->getStop() == dir){
-          dir=0;
+    void doMove(int delta){
+      if(delta!=0){
+        if(endstops->getStop()*delta > 0){
+          delta=0;
         }
       }
-      debug(dir);
-      ledcWrite(DRV8871_PWMCHANNEL1, dir<0?MAX_DUTY_CYCLE:0);
-      ledcWrite(DRV8871_PWMCHANNEL2, dir>0?MAX_DUTY_CYCLE:0);
+      debug(delta);
+      ledcWrite(DRV8871_PWMCHANNEL1, delta<0?(-delta>-MAX_DUTY_CYCLE, MAX_DUTY_CYCLE, -delta):0);
+      ledcWrite(DRV8871_PWMCHANNEL2, delta>0?(+delta>+MAX_DUTY_CYCLE, MAX_DUTY_CYCLE, +delta):0);
     }
     
     int dutyCycle;
     /* Setting PWM Properties */
     int MAX_DUTY_CYCLE = (int)(pow(2, DRV8871_PWMRESOLUTION) - 1);
-    int MIN_DUTY_CYCLE = MAX_DUTY_CYCLE*3/4;
+    int MIN_DUTY_CYCLE = MAX_DUTY_CYCLE*3/5;
   public:
     DRV8871(Endstops* endstops){
       Serial.print("init DRV8871. DEBUG=");
@@ -61,28 +60,38 @@ class DRV8871 {
       ledcSetup(DRV8871_PWMCHANNEL2, DRV8871_PWMFREQ, DRV8871_PWMRESOLUTION);
       ledcAttachPin(DRV8871_IN2, DRV8871_PWMCHANNEL2);
       // init end stops
-   }
+    }
+  
+    int getAbsMinValue(){
+      return MIN_DUTY_CYCLE;
+    }
 
-   void debug(int dir){
+    int getAbsMaxValue(){
+      return MAX_DUTY_CYCLE;
+    }
+
+    void debug(int dir){
       #ifdef DRV8871_DEBUG
         if(dir!=0){
           Serial.print("DRV8871 : ");
-          Serial.println(dir<0?"-":(dir>0?"+":"0"));
+          Serial.print(dir<0?"- ,":(dir>0?"+ ,":"0 ,"));
+          Serial.println(dir);
         }
       #endif
-   }
+    }
 
     void left(){
-      return doMove(-1);
+      return doMove(-MAX_DUTY_CYCLE);
     }
 
     void right(){
-      return doMove(1);
+      return doMove(MAX_DUTY_CYCLE);
     }
 
     void move(int dir){
       doMove(dir);
     }
+
     void stop(){
       return doMove(0);
     }
--- Arduino/AutoHelm_800/src/main.cpp
+++ Arduino/AutoHelm_800/src/main.cpp
@@ -1,10 +1,10 @@
 //#define MAX471_DEBUG
 //#define L298N_DEBUG
 
-// #define HEADING_DEBUG
-#define PID_DEBUG
+#define HEADING_DEBUG
+// #define PID_DEBUG
 #define KEYPAD_DEBUG
-// #define DRV8871_DEBUG
+#define DRV8871_DEBUG
 #define ENDSTOP_DEBUG
 
 // #define MPU9250_DEBUG
@@ -22,7 +22,7 @@
 #include "MPU9250.cpp" 
 #include <PID_v1.h>
 
-
+#define MAX_PILOT_VALUE 100
 AHKeypad* keyboard;
 DRV8871*  cylinder;
 AHServer* ahServer;
@@ -34,7 +34,7 @@ enum Mode {MODE_AUTO, MODE_STANDBY, MODE_GOTO, MODE_NONE};
 //Define Variables we'll be connecting to
 double pidSetpoint = 0, pidInput = 0, pidOutput;
 //Specify the links and initial tuning parameters
-double Kp=1, Ki=0.05, Kd=0.25;
+double Kp=4, Ki=3, Kd=2;
 PID myPID(&pidInput, &pidOutput, &pidSetpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
@@ -47,23 +47,38 @@ void setup() {
   ahServer = new AHServer();
   compass  = new MPU9250();
   myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(-100, 100);
+  myPID.SetOutputLimits(-MAX_PILOT_VALUE, MAX_PILOT_VALUE);
 }
 
 int targetHeading = 0;   // heading direction in degrees
-Mode mode = MODE_NONE;
+Mode mode = MODE_STANDBY;
+
+/**
+ * move motor according to motor limits. 
+ */
+void move(int delta){
+  if(delta>+MAX_PILOT_VALUE) delta = +MAX_PILOT_VALUE;
+  if(delta<-MAX_PILOT_VALUE) delta = -MAX_PILOT_VALUE;
+
+  cylinder->move(delta<0?map(delta, -MAX_PILOT_VALUE, 0, -cylinder->getAbsMaxValue(), -cylinder->getAbsMinValue())
+                :delta>0?map(delta, 0, MAX_PILOT_VALUE, cylinder->getAbsMinValue(), cylinder->getAbsMaxValue())
+                :0);
+}
 
 void loop() {
   int shipHeading = compass->getHeading();
   // keyboard
   AHKey key = keyboard->getKey();
   switch (key) {
-    case KEY_AUTO : 
+    case KEY_AUTO :
       mode = MODE_AUTO;
       targetHeading = shipHeading;
+      myPID.SetMode(MANUAL);
+      myPID.SetMode(AUTOMATIC);
       break;
     case KEY_STANDBY :
       mode = MODE_STANDBY;
+      targetHeading = 0;
       break;
     case KEY_PLUSONE :
       targetHeading += 1;
@@ -78,48 +93,53 @@ void loop() {
       targetHeading -= 10;
       break;
   }
-  // recale target entre 0 et 360°
-  if(targetHeading<0)   targetHeading += 360;
-  if(targetHeading>360) targetHeading -= 360;
+
+  if(mode==MODE_AUTO) {
+    // recale target entre 0 et 360°
+    if(targetHeading<0)   targetHeading += 360;
+    if(targetHeading>360) targetHeading -= 360;
+    
+    // deviation
+    int deltaHeading = targetHeading - shipHeading;
+
+    // recale déviation entre -180 et + 180
+    if(deltaHeading>180) deltaHeading = deltaHeading - 360;
+    if(deltaHeading<-180) deltaHeading = deltaHeading + 360;
+
+    #ifdef HEADING_DEBUG
+      if(deltaHeading != 0){
+        Serial.print("MODE : AUTO , Heading :");
+        Serial.print(targetHeading);
+        Serial.print(", Compass :");
+        Serial.print(shipHeading);  
+        Serial.print(", Delta :");
+        Serial.print(deltaHeading);
+        Serial.println();
+      }
+    #endif
+
+    pidInput = trunc(deltaHeading / 5) * 5;
+    myPID.Compute();
   
-  // deviation
-  int deltaHeading = targetHeading - shipHeading;
-
-  // recale déviation entre -180 et + 180
-  if(deltaHeading>180) deltaHeading = deltaHeading - 360;
-  if(deltaHeading<-180) deltaHeading = deltaHeading + 360;
-
-  pidInput = trunc(deltaHeading / 5) * 5;
-  myPID.Compute();
-
-  #ifdef HEADING_DEBUG
-    if(deltaHeading != 0){
-      Serial.print("MODE :");
-      Serial.print(mode);
-      Serial.print(", Heading :");
-      Serial.print(targetHeading);
-      Serial.print(", Compass :");
-      Serial.print(shipHeading);  
-      Serial.print(", Delta :");
-      Serial.print(deltaHeading);
+    #ifdef PID_DEBUG
+      Serial.print("PID Input : ");
+      Serial.print(pidInput);
+      Serial.print(", PID Output : ");
+      Serial.print(pidOutput);
       Serial.println();
-    }
-  #endif
-  #ifdef PID_DEBUG
-    Serial.print("PID Input : ");
-    Serial.print(pidInput);
-    Serial.print(", PID Output : ");
-    Serial.print(pidOutput);
-    Serial.println();
-  #endif
-  switch(mode){
-    case MODE_AUTO :
-      cylinder->move(pidOutput);
-      break;
-    case MODE_STANDBY : 
-      break;
-    case MODE_NONE:
-      break;
+    #endif
+
+    move(pidOutput);
+
+  } else if (mode == MODE_STANDBY) {
+      move(targetHeading);
+      #ifdef HEADING_DEBUG
+        if(targetHeading!=0) {
+          Serial.print("MODE : STANDBY, Heading :");
+          Serial.print(targetHeading);
+          Serial.println();
+        }
+      #endif
   }
   
   // cylinder->loop();
